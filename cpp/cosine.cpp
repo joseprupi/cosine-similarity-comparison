2d01af02afc0f036dcd24ab4cacd09653f337e9b
root <joseprupi@gmail.com>
2023-09-04 01:40:49 +0000

    SIMD

--- cpp/cosine.cpp
+++ cpp/cosine.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
-#include <iomanip>
-#include <string>
-#include <map>
-#include <random>
-#include <cmath>
-#include <iostream>
-#include <array>
-#include <iterator>
 #include <random>
 #include <algorithm>
 #include <chrono>
+#include <cassert>
+
+#include <immintrin.h>
 
 using namespace std;
 using namespace std::chrono;
 
-int SIZE = 10000000;
+int SIZE = 3200;
 
 double cosine_similarity(float *A, float *B)
 {
     float dot = 0.0, denom_a = 0.0, denom_b = 0.0;
-    for (unsigned int i = 0u; i < SIZE; ++i)
+    for (auto i = 0; i < SIZE; ++i)
     {
         dot += A[i] * B[i];
         denom_a += A[i] * A[i];
@@ -28,37 +23,80 @@ double cosine_similarity(float *A, float *B)
     return dot / (sqrt(denom_a) * sqrt(denom_b));
 }
 
+float cosine_similarity_simd(float *A, float *B)
+{
+
+    __m256 sum_dot = _mm256_setzero_ps();
+    __m256 sum_A = _mm256_setzero_ps();
+    __m256 sum_B = _mm256_setzero_ps();
+
+    for (size_t i = 0; i < SIZE; i += 8)
+    {
+        __m256 buf1 = _mm256_loadu_ps(A + i);
+        __m256 buf2 = _mm256_loadu_ps(B + i);
+
+        sum_dot = _mm256_fmadd_ps(buf1, buf2, sum_dot);
+        sum_A = _mm256_fmadd_ps(buf1, buf1, sum_A);
+        sum_B = _mm256_fmadd_ps(buf2, buf2, sum_B);
+    }
+
+    __m128 r4 = _mm_add_ps(_mm256_castps256_ps128(sum_dot), _mm256_extractf128_ps(sum_dot, 1));
+    __m128 r2 = _mm_add_ps(r4, _mm_movehl_ps(r4, r4));
+    __m128 r1 = _mm_add_ss(r2, _mm_movehdup_ps(r2));
+    const float float_dot = _mm_cvtss_f32(r1);
+
+    r4 = _mm_add_ps(_mm256_castps256_ps128(sum_A), _mm256_extractf128_ps(sum_A, 1));
+    r2 = _mm_add_ps(r4, _mm_movehl_ps(r4, r4));
+    r1 = _mm_add_ss(r2, _mm_movehdup_ps(r2));
+    const float float_A_norm = _mm_cvtss_f32(r1);
+
+    r4 = _mm_add_ps(_mm256_castps256_ps128(sum_B), _mm256_extractf128_ps(sum_B, 1));
+    r2 = _mm_add_ps(r4, _mm_movehl_ps(r4, r4));
+    r1 = _mm_add_ss(r2, _mm_movehdup_ps(r2));
+    const float float_B_norm = _mm_cvtss_f32(r1);
+
+    const float result = float_dot / (sqrt(float_A_norm) * sqrt(float_B_norm));
+    return result;
+}
+
 int main()
 {
-    std::random_device rd;
-    std::mt19937 e2(rd());
-    // std::minstd_rand0 g1(rd());
-    // std::subtract_with_carry_engine<unsigned, 24, 10, 24> g2(rd());
-    std::uniform_real_distribution<float> dist(0, 1);
 
-    // float* A = new float[SIZE];
-    // float* B = new float[SIZE];
-
-    // for (int n = 0; n < SIZE; ++n) {
-    //     A[n] = dist(e2);
-    //     B[n] = dist(e2);
-    // }
-
+    // Regular 
     std::vector<float> A(SIZE);
     std::vector<float> B(SIZE);
 
-    std::generate(A.begin(), A.end(), [&]()
-                  { return dist(e2); });
-    std::generate(B.begin(), B.end(), [&]()
-                  { return dist(e2); });
-
+    for (int i = 0; i < SIZE; i++)
+    {
+        A[i] = 1;
+        B[i] = i + 100;
+    }
 
     auto t1 = high_resolution_clock::now();
-    float sim = cosine_similarity(&A[0], &B[0]);
+    const float sim = cosine_similarity(&A[0], &B[0]);
     auto t2 = high_resolution_clock::now();
 
-    duration<double, std::milli> ms_double = t2 - t1;
-    std::cout << ms_double.count() << "ms\n";
+    std::cout << sim << "\n";
 
-    printf("%f", sim);
+    duration<double, std::milli> normal_duration = t2 - t1;
+
+    // SIMD
+    __attribute__((aligned(32))) float a[SIZE], b[SIZE];
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        a[i] = 1;
+        b[i] = i + 100;
+    }
+
+    t1 = high_resolution_clock::now();
+    const float sim_simd = cosine_similarity_simd(&a[0], &b[0]);
+    t2 = high_resolution_clock::now();
+
+    std::cout << sim_simd << "\n";
+
+    duration<double, std::milli> simd_duration = t2 - t1;
+
+    std::cout << normal_duration.count() << "ms\n";
+    std::cout << simd_duration.count() << "ms\n";
 }
